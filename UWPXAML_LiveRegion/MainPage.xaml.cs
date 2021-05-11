0a5a60c054eb794fdc411e687fb616259bd5258d
TheDoctor <TheDoctor@Gallifrey.org>
2021-05-11 08:41:16 -0700

    Add project files.

---
+++ UWPXAML_LiveRegion/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Automation.Peers;
+using Windows.UI.Xaml.Controls;
+
+namespace UWPXAML_LiveRegion
+{
+    public sealed partial class MainPage : Page
+    {
+        private int currentStatus = 1;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ChangeStatusButton_Click(object sender, RoutedEventArgs e)
+        {
+            LiveStatusTB.Text = "Status is now " + currentStatus.ToString();
+            ++currentStatus;
+
+            var peer = FrameworkElementAutomationPeer.FromElement(LiveStatusTB);
+            if (peer != null)
+            {
+                peer.RaiseAutomationEvent(AutomationEvents.LiveRegionChanged);
+            }
+        }
+    }
+}
